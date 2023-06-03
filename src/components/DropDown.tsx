52d35e27d58f626a1dfbd8b5502142c6d6aa590e
charlesfrisbee <char5653@uni.sydney.edu.au>
2023-06-04 01:59:21 +0200

    add filter dropdown

---
+++ src/components/DropDown.tsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+import { type DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu";
+
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { ChevronDown } from "lucide-react";
+
+type Checked = DropdownMenuCheckboxItemProps["checked"];
+
+export function Dropdown() {
+  const [showStatusBar, setShowStatusBar] = React.useState<Checked>(true);
+  const [showActivityBar, setShowActivityBar] = React.useState<Checked>(false);
+  const [showPanel, setShowPanel] = React.useState<Checked>(false);
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="flex gap-2 p-0">
+          Recents <ChevronDown className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuCheckboxItem
+          checked={showStatusBar}
+          onCheckedChange={setShowStatusBar}
+        >
+          Status Bar
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          checked={showActivityBar}
+          onCheckedChange={setShowActivityBar}
+          disabled
+        >
+          Activity Bar
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          checked={showPanel}
+          onCheckedChange={setShowPanel}
+        >
+          Panel
+        </DropdownMenuCheckboxItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,5 +1,6 @@
 import { ArrowRight, Home, Library, Plus, Search } from "lucide-react";
-import { Button } from "./Button";
+import { Button } from "./ui/button";
+import { Dropdown } from "./DropDown";
 
 const topItems = [
   {
@@ -14,31 +15,35 @@ const topItems = [
 
 const SideBar = () => {
   return (
-    <div className="flex h-screen w-2/12 flex-col gap-2 p-2">
-      <section className="rounded-md bg-slate-900/80 p-4" id="top-part">
-        <ul className=" flex flex-col gap-4 font-semibold">
+    <div className="flex h-screen w-2/12 min-w-fit flex-col gap-2 p-2">
+      <section className="rounded-md bg-slate-900/80 p-6" id="top-part">
+        <ul className=" flex flex-col gap-6 font-semibold">
           {topItems.map((item) => (
-            <li className="flex items-center gap-4" key={item.text}>
+            <li className="flex items-center gap-6" key={item.text}>
               <item.icon className="h-6 w-6" />
               <span>{item.text}</span>
             </li>
           ))}
         </ul>
       </section>
-      <section className="flex-1 rounded-md bg-slate-900/80 p-4" id="main-part">
-        <header className="flex items-center justify-between gap-4">
-          <div className="inline-flex gap-4">
+      <section
+        className="flex-1 gap-4 rounded-md bg-slate-900/80 p-6"
+        id="main-part"
+      >
+        <header className="flex items-center justify-between gap-6">
+          <div className="inline-flex gap-6">
             <Library />
             Your library
           </div>
-          <div className="inline-flex gap-4">
+          <div className="inline-flex items-center gap-6">
             <Plus />
             <ArrowRight />
           </div>
-          {/* <Button variant={"destructive"}>
-            <span>hey</span>
-          </Button> */}
         </header>
+        <section className="flex items-center justify-between pt-4">
+          <Search className="h-4 w-5" />
+          <Dropdown />
+        </section>
       </section>
     </div>
   );
